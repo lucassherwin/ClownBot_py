4d3f62c28346438dc02d43e858a5e68dd60d123a
lucassherwin <lksherwin@gmail.com>
2021-03-26 13:34:55 -0400

    updated oauth and added login route -- now clicking login button redirects to discord authorize page

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from logging import debug
 from flask import Flask, render_template
+from oauth import Oauth
 
 app = Flask(__name__)
 
 @app.route('/') # define our route
 def home():
-  return render_template('index.html') # renders index.html file from templates folder
+  return render_template('index.html', discord_url=Oauth.discord_login_url) # renders index.html file from templates folder
+
+@ app.route('/login')
+def login():
+  return 'Success'
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__': # only run if we are running the code
   app.run(debug=True) # allows code updates
--- oauth.py
+++ oauth.py
@@ -7,6 +7,6 @@ class Oauth:
   client_secret = os.getenv('DISCORD_SECRET')
   redirect_uri = 'http://127.0.0.1:5000/login'
   scope = 'identify%20email%20guilds'
-  discord_login_url = 'https://discord.com/api/oauth2/authorize?client_id=789975160517689374&redirect_uri=http%3A%2F%2F127.0.0.1%3A5000%2F&response_type=code&scope=identify%20email%20guilds'
+  discord_login_url = 'https://discord.com/api/oauth2/authorize?client_id=789975160517689374&redirect_uri=http%3A%2F%2F127.0.0.1%3A5000%2Flogin&response_type=code&scope=identify%20email%20guilds'
   discord_token_url = 'https://discord.com/api/oauth2/token'
   discord_api_url = 'https://discord.com/api'
