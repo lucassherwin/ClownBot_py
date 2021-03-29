b9a056879af183643a26fe0a8cf436a1ef3b9e77
lucassherwin <lksherwin@gmail.com>
2021-03-29 17:35:34 -0400

    we can now get passed the authorize screen -- issue was with the client secret env variable

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from logging import debug
 from quart import Quart, render_template, request, session, redirect, url_for
-from quart_discord import DiscordOAuth2Session
+from quart_discord import DiscordOAuth2Session, Unauthorized, requires_authorization
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 app = Quart(__name__)
-app.config['SECRET_KEY'] = 'test123'
+# app.config['SECRET_KEY'] = 'test123'
+app.secret_key = b'random string'
 app.config['DISCORD_CLIENT_ID'] = 789975160517689374
 app.config['DISCORD_CLIENT_SECRET'] = os.getenv('DISCORD_CLIENT_SECRET')
 app.config['DISCORD_REDIRECT_URI'] = 'http://127.0.0.1:5000/callback'
+app.config["DISCORD_BOT_TOKEN"] = "Nzg5OTc1MTYwNTE3Njg5Mzc0.X953sw.qWA1Xm0TKBlG7jIpbgDEWlCRxy8"
 
 discord = DiscordOAuth2Session(app)
 
@@ -21,10 +27,14 @@ async def login():
 
 @app.route('/callback')
 async def callback():
-  try:
-    await discord.callback()
-  except:
-    return redirect(url_for('login'))
+  await discord.callback()
+  # return redirect(url_for('home'))
+  # try:
+  #   print('in try')
+  #   await discord.callback()
+  # except:
+  #   print('in except')
+  #   return redirect(url_for('login'))
 
   user = await discord.fetch_user()
   return f'{user.name}#{user.discriminator}'
